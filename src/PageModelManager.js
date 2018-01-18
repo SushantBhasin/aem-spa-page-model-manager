1b5650bc0f2143afa7eee7340fcb74e19df4d129
fauchere <fauchere@adobe.com>
2018-01-18 08:27:39 +0100

    CQ-4228254 - [Editable SPA] Make the PageModelManager project an NPM … (#1)
    * CQ-4228254 - [Editable SPA] Make the PageModelManager project an NPM package
    
    - project configuration
    - PageModelManager as a module
    - Utils library as a module
    - CustomEvent polyfill as a npm package dependency
    - unit test setup
    - npm package setup
    - jshint setup
    
    * CQ-4228254 - [Editable SPA] Make the PageModelManager project an NPM package
    
    - rewrite the page model manager to be exportable as a default singleton
    - got rid of unnecessary event handlers
    - rename functions for readability of the API
    - use npm clone component for deep copy, got rid of the utils extend
    - add unit tests to test the immutability of the page model
    
    * CQ-4228254 - [Editable SPA] Make the PageModelManager project an NPM package
    
    - rewrite the page model manager to be exportable as a default singleton
    - got rid of unnecessary event handlers
    - rename functions for readability of the API
    - use npm clone component for deep copy, got rid of the utils extend
    - add unit tests to test the immutability of the page model
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- .gitignore and .npmignore
    -- refactor
    -- lint config
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- private functions are now private
    -- jsdoc generation added (dependency, script)
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- added README file (copy paste from jsdocs)
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- private functions are private in module by default
    -- jsdoc generation corrected
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- changes after tests
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- basic tests
    -- code coverage reporter
    -- whitespace correction
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- rename of a function
    
    * CQ-4228080 = Write Unit tests for PageModelManager
    -- rename of a function
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- fixes after review
    
    * CQ-4228254 = [Editable SPA] Make the PageModelManager project an NPM package
    -- setData made private + test adaptation
    
    * CQ-4228254 - [Editable SPA] Make the PageModelManager project an NPM package
    
    - code coverage dependencies

---
+++ src/PageModelManager.js
@@ -0,0 +1,423 @@
+/*
+ * ADOBE CONFIDENTIAL
+ *
+ * Copyright 2017 Adobe Systems Incorporated
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of Adobe Systems Incorporated and its suppliers,
+ * if any.  The intellectual and technical concepts contained
+ * herein are proprietary to Adobe Systems Incorporated and its
+ * suppliers and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from Adobe Systems Incorporated.
+ */
+import clone from 'clone';
+
+const ITEMS_PN = ':items';
+const ITEMS_ORDER_PN = ':itemsOrder';
+
+/**
+ * Contains the Page Model Object.
+ * It is set via {@code PageModelManager#init}
+ * @private
+ */
+let pageModel;
+
+/**
+ * Map of paths and their registered listeners
+ * @private
+ */
+let listeners = {};
+
+/**
+ * Call listeners on the given path
+ *
+ * @param {string} path - Path
+ * @private
+ */
+function notifyListeners(path) {
+    let listenersForPath = listeners[path];
+
+    if (listenersForPath) {
+        listenersForPath.forEach(function(listener) {
+            try {
+                listener();
+            }
+            catch (e) {
+                console.warn('Error in listener at path ' + path + ' : ' + e);
+            }
+        });
+    }
+}
+
+/**
+ * Fires the @event 'cq-pagemodel-loaded' with the needed data
+ *
+ * @fires Window#cq-pagemodel-loaded
+ * @private
+ */
+function triggerPageModelLoaded() {
+    // Deep copy to protect the internal state of the page model
+    window.dispatchEvent(new CustomEvent('cq-pagemodel-loaded', {
+        detail: {
+            model: clone(pageModel)
+        }
+    }));
+}
+
+/**
+ * Returns the child Model at the given path from the given PageModel
+ *
+ * @param {string} path  Path to the child object
+ * @param {{}} model     Parent model object
+ * @private
+ */
+function getModelChild(path, model) {
+    if (!model || !path) {
+        console.error('PageModelManager.js', 'Cannot introspect model', path, model);
+        return;
+    }
+
+    let elements = path.split('/');
+
+    let child = model[ITEMS_PN][elements.shift()];
+
+    if (!child || elements.length < 1) {
+        return child;
+    }
+
+    return getModelChild(elements.join("/"), child);
+}
+
+/**
+ * Resolves the model for the given path
+ *
+ * @param {string} [path]           Path to the child item
+ * @param {{}} pageModel            Model of the page
+ * @param {function} resolveFnc     Function to be called with the resolved model
+ * @param {boolean} [immutable=true]     Should a returned pageModel be a copy
+ * @private
+ */
+function resolveModel(path, pageModel, resolveFnc, immutable) {
+    let model;
+    // Is the path empty
+    if (!path || path.trim().length < 1) {
+        model = immutable !== false ? clone(pageModel) : pageModel;
+        resolveFnc(model);
+
+        return;
+    }
+
+    model = getModelChild(path, pageModel);
+
+    resolveFnc(immutable !== false ? clone(model) : model);
+}
+
+/**
+ * Splits the path into parent path and the node name as key
+ * If the path is '/A/B/C', it will return {'parent': '/A/B', 'key': 'C'}
+ *
+ * @param {string} path - Path string
+ * @returns {{}} Returns an Object containing parent path mapped to 'parent'
+ *               and key name mapped to 'key'
+ * @private
+ */
+function splitParentPath(path) {
+    let elements = path.split('/');
+    let key = elements.pop();
+    let parent = elements.join('/');
+    return { 'parent': parent, 'key': key };
+}
+
+/**
+ * Updates the PageModel with the provided data
+ *
+ * @fires Window#cq-pagemodel-loaded
+ * @param {UpdateEvent.detail.msg} msg - Object containing the data to update the PageModel
+ * @private
+ */
+function updateModel(msg) {
+    if (!msg || !msg.cmd || !msg.path) {
+        console.error('PageModelManager.js', 'Not enough data received to update the Page Model');
+        return;
+    }
+    // Path in the PageModel which needs to be updated
+    let path = msg.path;
+    // Command Action requested via Editable on the content Node
+    let cmd = msg.cmd;
+    // Data that needs to be updated in the PageModel at {path}
+    let data = clone(msg.data);
+
+    // Variable to hold the Promise Object
+    let promise;
+
+    switch(cmd) {
+        case 'replace':
+            promise = setData.call(this, path, data);
+            break;
+
+        // If we ever start using it via the EditableActions in future
+        // case 'replaceContent':
+
+        case 'insertBefore':
+            promise = insertPath.call(this, path, data, true);
+            break;
+
+        case 'insertAfter':
+            promise = insertPath.call(this, path, data, false);
+            break;
+
+        case 'moveBefore':
+            promise = movePath.call(this, path, data, true);
+            break;
+
+        case 'moveAfter':
+            promise = movePath.call(this, path, data, false);
+            break;
+
+        case 'delete':
+            promise = deletePath.call(this, path);
+            break;
+    }
+
+    promise.then(triggerPageModelLoaded);
+}
+
+/**
+ * Delete data at given path from the Page Model
+ *
+ * @param {string} path
+ * @returns {Promise}
+ * @private
+ */
+function deletePath(path) {
+    let p = splitParentPath(path);
+
+    return this.getData(p.parent, false).then(function(data) {
+        return new Promise(function (resolve) {
+
+        delete data[ITEMS_PN][p.key];
+        let index = data[ITEMS_ORDER_PN].indexOf(p.key);
+        data[ITEMS_ORDER_PN].splice(index, 1);
+        notifyListeners(p.parent);
+
+            resolve();
+        });
+    });
+}
+
+/**
+ * Insert the new data structure before/after the given path in the Page Model
+ *
+ * @param {string} path - The path in the Page Model to be updated
+ * @param {{}} data - New Data Object to be set
+ * @param {boolean} insertBefore - 'true' to insert new data before the path, 'false' otherwise
+ * @returns {Promise}
+ * @private
+ */
+function insertPath(path, data, insertBefore) {
+    let parentAndKey = splitParentPath(path);
+
+    return this.getData(parentAndKey.parent, false).then(function(parentData) {
+        // Insert the new data item as a sibling of the item at given path
+        let localData = clone(data);
+        parentData[ITEMS_PN][localData.key] = localData.value;
+
+        // Set order of the new item
+        let index = parentData[ITEMS_ORDER_PN].indexOf(parentAndKey.key);
+
+        if (index > -1) {
+            parentData[ITEMS_ORDER_PN].splice(insertBefore ? index : index + 1, 0, localData.key);
+        } else {
+            parentData[ITEMS_ORDER_PN].push(localData.key);
+        }
+
+        notifyListeners(parentAndKey.parent);
+    });
+}
+
+/**
+ * Move data from {path} to {data.key} before/after {data.sibling}
+ *
+ * @param {string} path - The path from where the data has to be moved
+ * @param {{}} data
+ * @param {boolean} insertBefore - Set to 'true' to move data before {data.sibling}, 'false' otherwise
+ * @returns {Promise}
+ * @private
+ */
+function movePath(path, data, insertBefore) {
+    let self = this;
+
+    return this.getData(path, false).then(function (nodeData) {
+
+        return deletePath.call(self, path).then(function() {
+            let localData = clone(data);
+            let insertData = {
+                key: localData.key,
+                value: nodeData
+            };
+
+            return insertPath.call(self, localData.sibling, insertData, insertBefore);
+        });
+    });
+}
+
+/**
+ * Set newData at the given path in the PageModel
+ *
+ * @param {string} path - Path in the PageModel at which to set new data
+ * @param {Object} newData - New Data Object to be set
+ * @returns {Promise}
+ */
+function setData(path, newData) {
+    let splitPaths = splitParentPath(path);
+
+    return this.getData(splitPaths.parent, false).then(function (data) {
+        if (data[ITEMS_PN]) {
+            let localData = clone(newData);
+            data[ITEMS_PN][splitPaths.key] = localData.value;
+
+            notifyListeners(path);
+        }
+    });
+}
+
+/**
+ * The {@code PageModelManager} is responsible for centralizing, synchronizing and providing access to the model of the page
+ * @exports PageModelManager
+ *
+ * @type {{init: PageModelManager.init, getData: PageModelManager.getData, setData: PageModelManager.setData, addListener: PageModelManager.addListener, removeListener: PageModelManager.removeListener}}
+ */
+const PageModelManager = {
+
+    /**
+     * Triggered by the PageModelManager to indicate that the PageModel for the page was loaded (fetched/updated)
+     *
+     * @event Window#cq-pagemodel-loaded
+     */
+
+    /**
+     * Event object containing the updated model
+     * @typedef {Object} UpdateEvent
+     * @property {Object} detail
+     * @property {Object} detail.msg -
+     * @property detail.msg.path - Path in the PageModel which needs to be updated
+     * @property detail.msg.cmd - Command Action requested via Editable on the content Node
+     * @property detail.msg.data - Data that needs to be updated in the PageModel at {path}
+     */
+
+    /**
+     * The PageModel for the page should be updated using the data passed in the event
+     * @type {UpdateEvent}
+     * @event Window#cq-pagemodel-update
+     */
+
+    /**
+     * Fetch the Page Model from the server.
+     *
+     * @fires Window#cq-pagemodel-loaded
+     * @param {string} path
+     * @returns {Promise} Promise resolved by PageModel
+     */
+    init: function(path) {
+        return new Promise(function(resolve, reject) {
+
+            if (!path) {
+                console.warn('PageModelManager.js', 'Fetching model rejected for path:', path);
+
+                reject();
+            }
+
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', path);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.setRequestHeader('Accept', 'application/json');
+
+            xhr.onload = function() {
+                if (xhr.status >= 400) {
+                    console.error('Problem with request: ${res.statusCode}');
+                } else {
+                    listeners = {};
+                    pageModel = JSON.parse(this.responseText);
+                    resolve(clone(pageModel));
+
+                    triggerPageModelLoaded();
+                }
+            };
+            xhr.send();
+        });
+    },
+
+    /**
+     * Extracts the data stored in the pageModel at the given path and returns a Promise
+     * resolved by that data
+     *
+     * @param {string}  [path]              Path to the child item
+     * @param {boolean} [immutable=true]    Should a returned pageModel be a copy
+     * returns {Promise}
+     */
+    getData: function(path, immutable) {
+        return new Promise(function (resolve) {
+            if (pageModel) {
+                resolveModel(path, pageModel, resolve, immutable);
+            } else {
+                // Wait for the page model to be available
+                let retryResolveModel = function retryResolveModel() {
+                    if (!pageModel) {
+                        requestAnimationFrame(retryResolveModel);
+                        return;
+                    }
+
+                    resolveModel(path, pageModel, resolve, immutable);
+                };
+
+                requestAnimationFrame(retryResolveModel);
+            }
+        });
+    },
+
+    /**
+     * Add {callback} as a listener for changes at {path}
+     *
+     * @param {string} path - Path
+     * @param {Function} callback - Function to be executed listening to changes at given path
+     */
+    addListener: function(path, callback) {
+        listeners[path] = listeners[path] || [];
+        listeners[path].push(callback);
+    },
+
+    /**
+     * Remove {callback} listener from {path}
+     * 
+     * @param {string} path - Path
+     * @param {Function} callback - Listener function to be removed  from path
+     */
+    removeListener: function(path, callback) {
+        let listenersForPath = listeners[path];
+
+        if (listenersForPath) {
+            let index = listenersForPath.indexOf(callback);
+            if (index !== -1) {
+                listenersForPath.splice(index, 1);
+            }
+        }
+    }
+};
+
+/**
+ * Entry point to update the page model from an external source written in es5 such as the page editor source code
+ */
+window.addEventListener('cq-pagemodel-update', function(event) {
+    if (!event || !event.detail || !event.detail.msg) {
+        console.warn('PageModelManager.js', 'No message passed to cq-pagemodel-update', event);
+        return;
+    }
+
+    updateModel.call(PageModelManager, event.detail.msg);
+});
+
+export default PageModelManager;
---
+++ tests/PageModelManager.test.js
@@ -0,0 +1,329 @@
+import PageModelManager from '../dist/PageModelManager';
+
+describe('PageModelManager', () => {
+
+    const ITEMS_META_DATA = ':items';
+
+    const ITEMS_ORDER_META_DATA = ':itemsOrder';
+
+    const TYPE_META_DATA = ':type';
+
+    const STATIC_PAGE_MODEL = 'react-page.json';
+
+    const ERROR_VALUE = 'error';
+
+    const PAGE_MODEL_JSON = {
+        "designPath": "/libs/settings/wcm/designs/default",
+        "title": "React sample page",
+        "lastModifiedDate": 1512116041058,
+        "templateName": "sample-template",
+        "cssClassNames": "page",
+        "language": "en-US",
+        ":itemsOrder": [
+            "root"
+        ],
+        ":items": {
+            "root": {
+                "gridClassNames": "aem-Grid aem-Grid--12 aem-Grid--default--12",
+                "columnCount": 12,
+                ":itemsOrder": ["child0000", "child0001"],
+                ":items": {
+                    "child0000": {
+                        "gridClassNames": "aem-Grid aem-Grid--12 aem-Grid--default--12",
+                        "columnCount": 12,
+                        ":itemsOrder": ["child0010", "child0011"],
+                        ":items": {
+                            "child0010": {":type": "test/components/componentchild0"},
+                            "child0011": {":type": "test/components/componentchild1"}
+                        },
+                        ":type": "wcm/foundation/components/responsivegrid"
+                    },
+                    "child0001": {":type": "test/components/componentchild1"}
+                },
+                ":type": "wcm/foundation/components/responsivegrid"
+            }
+        },
+        ":type": "we-retail-react/components/structure/page"
+    };
+
+    const PARENT_PATH = 'root/child0000';
+    const SIBLING_PATH = PARENT_PATH + '/child0010';
+    const SIBLING_2_PATH = PARENT_PATH + '/child0011';
+
+    const CHILD_TYPE = 'test/components/componentChildType';
+    const CHILD_KEY = 'childXXXX';
+
+    const generateEventData = function ged(cmd) {
+        return { detail: { msg: {
+            cmd: cmd,
+            path: SIBLING_PATH,
+            data: {
+                key: CHILD_KEY,
+                value: {
+                    ':type': CHILD_TYPE
+                }
+            }
+        }}};
+    };
+
+    const HELPER_dispatchEventInsert = function dei(cmd) {
+
+        const eventData = generateEventData(cmd);
+
+        window.dispatchEvent(new CustomEvent('cq-pagemodel-update', eventData));
+    };
+
+    const HELPER_dispatchEventDelete = function ded() {
+
+        const eventData = generateEventData('delete');
+
+        window.dispatchEvent(new CustomEvent('cq-pagemodel-update', eventData));
+    };
+
+    const HELPER_dispatchEventMove = function dem(cmd, keyOfTheMovedNode, pathOfTheSibling) {
+
+        const eventData = { detail: { msg: {
+            cmd: cmd,
+            path: SIBLING_PATH,
+            data: {
+                key: keyOfTheMovedNode,
+                sibling: pathOfTheSibling
+            }
+        }}};
+
+        window.dispatchEvent(new CustomEvent('cq-pagemodel-update', eventData));
+    };
+
+
+    let server;
+
+    beforeEach(() => {
+        server = sinon.fakeServer.create();
+
+        server.respondWith("GET", STATIC_PAGE_MODEL,
+            [200, { "Content-Type": "application/json" }, JSON.stringify(PAGE_MODEL_JSON)]);
+
+        PageModelManager.init(STATIC_PAGE_MODEL).then(model => {
+            assert.deepEqual(PAGE_MODEL_JSON, model, 'Returns the page model object');
+        });
+
+        server.respond();
+    });
+
+    afterEach(() => {
+        server.restore();
+    });
+
+    describe('fetching a page model ->', () => {
+
+        it('should fail when no path has been provided', done => {
+            PageModelManager.init().then(() => {
+                done(false, 'No page model should be provided');
+            }, () => {
+                // Should result in an error
+                done();
+            });
+        });
+
+        it('should get an immutable page model', done => {
+            PageModelManager.init(STATIC_PAGE_MODEL).then(model1 => {
+                model1[TYPE_META_DATA] = ERROR_VALUE;
+
+                PageModelManager.getData().then(model2 => {
+                    assert.notEqual(ERROR_VALUE, model2[TYPE_META_DATA], 'The page model is mutable');
+                    done();
+                });
+            });
+
+            server.respond();
+        });
+
+    });
+
+    describe('getting a page model ->', () => {
+
+        it('should get an immutable model', done => {
+            PageModelManager.getData('root').then(model1 => {
+                model1[TYPE_META_DATA] = ERROR_VALUE;
+
+                PageModelManager.getData('root').then(model2 => {
+
+                    assert.notEqual(model1[TYPE_META_DATA], model2[TYPE_META_DATA], 'The model is mutable');
+                    done();
+                });
+            });
+        });
+
+        it('should get a specific page model child', done => {
+            PageModelManager.getData('root/child0000').then(pageModel => {
+                assert.deepEqual(PAGE_MODEL_JSON[ITEMS_META_DATA].root[ITEMS_META_DATA].child0000, pageModel, 'Returns the page model child object');
+                done();
+            });
+        });
+
+    });
+    describe('dispatch Event cq-pagemodel-update with message', () => {
+
+        describe('should end with an console if', () => {
+            it('no cmd passed', () => {
+                let spy = sinon.spy(console, 'error');
+
+                let eventData = { detail: { msg: { }}};
+                window.dispatchEvent(new CustomEvent('cq-pagemodel-update', eventData));
+
+                assert(spy.calledOnce);
+
+                // restore the original function
+                spy.restore();
+
+            });
+
+            it('event empty', () => {
+
+                let spy = sinon.spy(console, 'warn');
+
+                window.dispatchEvent(new CustomEvent('cq-pagemodel-update', {}));
+
+                assert(spy.calledOnce);
+
+                // restore the original function
+                spy.restore();
+            });
+
+        });
+
+        describe('"insert" should insert childXXXX', () => {
+
+            it('before child0010', done => {
+
+                PageModelManager.addListener(PARENT_PATH, () => {
+                    PageModelManager.getData(PARENT_PATH).then(pageModel => {
+
+                        assert.equal(
+                            pageModel[ITEMS_META_DATA][CHILD_KEY][TYPE_META_DATA],
+                            CHILD_TYPE);
+
+                        const order = pageModel[ITEMS_ORDER_META_DATA];
+                        assert.equal(
+                            order.indexOf('childXXXX') + 1,
+                            order.indexOf('child0010'));
+
+                        done();
+                    });
+                });
+
+                HELPER_dispatchEventInsert('insertBefore');
+
+            });
+
+            it('after child0010', done => {
+                PageModelManager.addListener(PARENT_PATH, () => {
+                    PageModelManager.getData(PARENT_PATH).then(pageModel => {
+
+                        assert.equal(
+                            pageModel[ITEMS_META_DATA][CHILD_KEY][TYPE_META_DATA],
+                            CHILD_TYPE);
+
+                        const order = pageModel[ITEMS_ORDER_META_DATA];
+                        assert.equal(
+                            order.indexOf('childXXXX') - 1,
+                            order.indexOf('child0010'));
+
+                        done();
+                    });
+                });
+
+                HELPER_dispatchEventInsert('insertAfter');
+
+            });
+        });
+
+        describe('"move" shoud move', () => {
+            it('child0010 after child0011', done => {
+
+                PageModelManager.addListener(PARENT_PATH, () => {
+                    PageModelManager.getData(PARENT_PATH).then(pageModel => {
+
+                        const order = pageModel[ITEMS_ORDER_META_DATA];
+                        assert.equal(
+                            order.indexOf('child0010') + 1,
+                            order.indexOf('child0011'));
+
+                        done();
+                    });
+                });
+
+                HELPER_dispatchEventMove('moveAfter', 'child0010', SIBLING_2_PATH);
+
+            });
+
+            it('child0011 before child0010', done => {
+
+                PageModelManager.addListener(PARENT_PATH, () => {
+                    PageModelManager.getData(PARENT_PATH).then(pageModel => {
+
+                        const order = pageModel[ITEMS_ORDER_META_DATA];
+                        assert.equal(
+                            order.indexOf('child0010') + 1,
+                            order.indexOf('child0011'));
+
+                        done();
+                    });
+                });
+
+                HELPER_dispatchEventMove('moveBefore', 'child0011', SIBLING_PATH);
+
+            });
+        });
+
+        it('"delete" shoudl delete child0010', done => {
+            PageModelManager.addListener(PARENT_PATH, () => {
+                PageModelManager.getData(PARENT_PATH).then(pageModel => {
+
+                    assert.equal(
+                        pageModel[ITEMS_ORDER_META_DATA].indexOf('child0010'),
+                        -1);
+
+                    done();
+                });
+            });
+
+            HELPER_dispatchEventDelete();
+
+        });
+
+        it('"replace" should replace child0010', done => {
+
+            PageModelManager.addListener(SIBLING_PATH, () => {
+                PageModelManager.getData(SIBLING_PATH).then(pageModel => {
+
+                    assert.equal(
+                        pageModel[TYPE_META_DATA],
+                        CHILD_TYPE);
+
+                    done();
+                });
+            });
+
+            const eventData = generateEventData('replace');
+
+            window.dispatchEvent(new CustomEvent('cq-pagemodel-update', eventData));
+
+        });
+    });
+
+
+    describe('Listeners ->', () => {
+        it('registered listener is notified after a cq-pagemodel-update', done => {
+            // note that this is check for only one of the possible cmds
+            // all cmds are tested in the cmds section
+            PageModelManager.addListener(PARENT_PATH, () => {
+                done();
+            });
+
+            HELPER_dispatchEventInsert('insertAfter');
+        });
+
+    });
+
+});
