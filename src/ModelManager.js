1ebd7d179020a92fb93ac1dfde8b6e5cae586d13
fauchere <fauchere@adobe.com>
2019-03-06 17:49:47 +0100

    @trivial changelog, comment
    Signed-off-by: fauchere <fauchere@adobe.com>

--- src/ModelManager.js
+++ src/ModelManager.js
@@ -146,6 +146,7 @@ class ModelManager {
      * Once the initial model is loaded and if the data model doesn't contain the path of the current pathname, the library attempts to fetch a fragment of model.
      *
      * @param {string|InitializationConfig} [config]                - URL to the data model or configuration object
+     * @fires cq-pagemodel-loaded
      * @return {Promise}
      */
     initialize(config) {
